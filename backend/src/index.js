b3a9a4359f142cc5cdf82b07b91418a010c10c12
KimDongKyu <daeggu87@gmail.com>
2018-02-17 04:22:04 +0900

    edit backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,13 +9,14 @@ const serve = require('koa-static');
 const path = require('path');
 const app = new Koa();
 const ssr = require('./ssr');
+const staticPath = path.join(__dirname, '../../frontend/build');
 
 app.use((ctx, next) => {
       if(ctx.path === '/') return ssr(ctx);
       return next();
 });
 
-app.use(serve(path.resolve(__dirname, '../../frontend/build/')));
+app.use(serve(staticPath));
 app.use(ssr);
 app.listen(port, ()=>{
       console.log(`server is listening to port ${port}`)
--- backend/src/ssr/index.js
+++ backend/src/ssr/index.js
@@ -1,16 +1,35 @@
-const fs = require('fs');
-const path = require('path');
 const render = require('./render').default;
 var serialize = require('serialize-javascript')
 
-const template = fs.readFileSync(path.join(__dirname,
-                '../../../frontend/build/index.html'), { encoding: 'utf8'});
+const manifest = require('../../../frontend/build/asset-manifest.json');
+
+function buildHtml({html, state}){
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+        <meta name="theme-color" content="#000000">
+        <link rel="manifest" href="/manifest.json">
+        <link rel="shortcut icon" href="/dk.ico">
+        <title>Daeggu</title>
+        <link href="/${manifest['app.css']}" rel="stylesheet">
+    </head>
+    <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <div id="root">${html}</div>
+        <script>
+            window.__PRELOADED_STATE__=${serialize(state)}
+        </script>
+        <script type="text/javascript" src="/${manifest['vendor.js']}"></script>
+        <script type="text/javascript" src="/${manifest['app.js']}"></script>
+    </body>
+    </html>
+    `;
+}
 
 module.exports = async (ctx) => {
-    const location = ctx.path;
-    const { html, state } = await render(location);
-    const page = template.replace('<div id="root"></div>', 
-    `<div id="root">${html}</div>
-    <script>window.__PRELOADED_STATE__=${serialize(state)}</script>`);
-    ctx.body = page; 
+    const rendered = await render(ctx.path);
+    ctx.body = buildHtml(rendered);  
  }
